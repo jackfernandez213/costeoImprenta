3f29a585f1e7e1cebc4a8cd15c200a2d0e92d115
jack <jackfernandez213@gmail.com>
2019-12-19 18:57:42 -0500

    agregando

--- js/main.js
+++ js/main.js
@@ -2,7 +2,23 @@
 
 window.addEventListener("load",function(){
 	// body...
-	var titulo = document.querySelector("titulo");
-	titulo.style.background = #536895;
+	var titulo = document.querySelector("#titulo");
+	titulo.style.background = "#536895";
+	titulo.style.color = "white";
+	titulo.innerHTML = "COSTEO DE TRABAJOS";
+	titulo.className = "clasHOLA";
+	var colorLetras = document.querySelector(".clasHOLA");
+	colorLetras.style.padding = "10px";
+
+	var formulario = document.getElementById("#formulario");
+	
+	var todosInputs = document.getElementsByTagName("h1");
+	var contitu = todosInputs[0].textContent;
+	console.log(contitu);
+
+	function cambiarNombre(nombre){
+		
+	}
+
 });
 
